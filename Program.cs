4abed67c036d55019e17b6977fe8ea4064690096
Joshua Vedane <jvedane@gmail.com>
2021-04-05 08:32:43 -0500

    inital commit

---
+++ Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+// Namespace
+namespace NumberGuesser
+{
+    // Main Class
+    class Program
+    {
+        // Entry Point Method
+        // Void isn't going to return anything.
+        // Static - static means a ref to the class itself, not instantiated
+        static void Main(string[] args)
+        {
+            // Set App Vars
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Joshua Vedane";
+
+            // Change text color
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            //Write out app info
+            Console.WriteLine($"{appName}: Version {appVersion} by {appAuthor}");
+
+            //Reset Text Color
+            Console.ResetColor();
+
+            // Ask users name
+            Console.WriteLine("What is your name?");
+
+            // Get user input
+            string inputName = Console.ReadLine();
+
+            Console.WriteLine($"Hello {inputName}, let's play a game...");
+
+            while (true)
+            {
+
+
+
+                // Set correct number
+                //int correctNumber = 7;
+
+                //Create a new Random object
+                Random random = new Random();
+                int correctNumber = random.Next(1, 10);
+
+                // init guess var
+                int guess = 0;
+
+                //Ask user for a number
+                Console.WriteLine("Guess a number between 1 and 10");
+
+                while (guess != correctNumber)
+                {
+                    // Get user's input
+                    string inputNumber = Console.ReadLine();
+
+                    //Make sure it's a number
+                    if (!int.TryParse(inputNumber, out guess))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"Please enter a real number, numbskull");
+                        Console.ResetColor();
+
+                        //keep going if good.
+                        continue;
+                    }
+
+                    // Cast to int and put in guess
+                    guess = Int32.Parse(inputNumber);
+
+                    // Match guess to correct number
+                    if (guess != correctNumber)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"Wrong Number, please try again");
+                        Console.ResetColor();
+                    }
+                }
+
+                // Output Success Message
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"Correct! You have won the game");
+                Console.ResetColor();
+
+                // ask to play again
+                Console.WriteLine("Play Again? [Y or N]?");
+
+                //Get answer
+                string answer = Console.ReadLine().ToUpper();
+
+                if (answer == "Y")
+                {
+                    continue;
+                }else if(answer == "N") {
+                    return;
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Number Guesser 
+This is a simple console application built in .NET and written in C#. I created it through Visual Studio following along with the Traversy Media tutorial here : https://www.youtube.com/watch?v=GcFJjpMFJvI&ab_channel=TraversyMedia . 
+
+Once the program is running, it will open up a window in the terminal. Follow the prompts for playing the game. The program generates a random number for each iteration of the game between 1 and 10. 
+
+## Getting Started 
+
+1. Fork and Clone this repo to your local machine. 
+2. Install .NET Core(3.1) : https://dotnet.microsoft.com/download
+3. Open the Project in your preferred IDE/Text Editor.
+4. Open a terminal and navigate to where this project is stored.
+5. Run ``` dotnet run ``` in your terminal. 
+
+### Notes 
+This is my first .NET/C# application. I started learning some basic C# from the Microsoft tutorials on April 1, 2020. 
+I found the Traversy tutorial on April 5, 2020. 
